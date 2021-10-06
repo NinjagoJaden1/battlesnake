508430ac511f2baebcd2c61f5e5946b8cbdc0bb5
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2021-10-06 15:59:11 -0700

    Create jaden3.js

---
+++ ai-p/jaden3.js
@@ -0,0 +1,11 @@
+function jaden3_getDirection(gridSize,snake,apples,direction){
+
+}
+function jaden3_newGame(){
+
+    return;
+}
+ais['jaden3'] = {
+    getDirection: jaden3_getDirection,
+    newGame: jaden3_newGame
+}
